8e5d1084f4fe9efb88acb234143161cb59fabb34
Justin Hall <justmhall@gmail.com>
2023-02-15 22:04:06 -0500

    Add test results and refactored smells

--- GildedRose-Refactoring-Kata-main/python/gilded_rose.py
+++ GildedRose-Refactoring-Kata-main/python/gilded_rose.py
@@ -1,40 +1,22 @@
 # -*- coding: utf-8 -*-
 
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.item_types = {
+            "Aged Brie":Aged_Brie,
+            "Backstage passes to a TAFKAL80ETC concert":Backstage_Pass,
+            "Sulfuras, Hand of Ragnaros":Base_Item
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            item_class = self.item_types.get(item.name, Generic_Item)
+            item_class(item).update()
+            
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -44,3 +26,48 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class Base_Item:
+    def __init__(self, item):
+        self.item = item
+    
+    def update(self):
+        return
+
+class Aged_Brie(Base_Item):
+    def update(self):
+        self.item.quality += 1
+        self.item.sell_in -= 1
+
+        if(self.item.sell_in < 0):
+            self.item.quality += 1
+
+        if(self.item.quality > 50):
+            self.item.quality = 50
+
+class Backstage_Pass(Base_Item):
+    def update(self):
+        self.item.sell_in -= 1
+        self.item.quality += 1
+
+        if(self.item.sell_in < 10):
+            self.item.quality += 1
+        
+        if(self.item.sell_in < 5):
+            self.item.quality += 1
+        
+        if(self.item.sell_in < 0):
+            self.item.quality = 0
+        return
+
+class Generic_Item(Base_Item):
+    def update(self):
+        self.item.sell_in -= 1
+        self.item.quality -= 1
+
+        if(self.item.sell_in < 0):
+            self.item.quality -= 1
+
+        if(self.item.quality < 0):
+            self.item.quality = 0
+        return
